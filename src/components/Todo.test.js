17ea09d49587043f3c601356ccecf33e2f246edc
Filipe Verrone de Lima <filipeverrone@lenovo.lan>
2020-09-23 14:41:36 -0300

    Test Project

---
+++ src/components/Todo.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { render, waitForElement, fireEvent } from "@testing-library/react";
+
+import Todo from "./Todo";
+
+describe("Tests for Todo component", () => {
+  it("Should add new task when form has been submitted", async () => {
+    // render the component
+    const { getByTestId, getByText } = render(<Todo />);
+
+    // search the input
+    const newTask = "testing";
+    const fieldNode = await waitForElement(() => getByTestId("form-field"));
+
+    // type in input
+    fireEvent.change(fieldNode, { target: { value: newTask } });
+    expect(fieldNode.value).toEqual(newTask);
+
+    // search the button
+    const btnNode = await waitForElement(() => getByTestId("form-btn"));
+
+    // click on button
+    fireEvent.click(btnNode);
+
+    // search the table
+    const tdNode = await waitForElement(() => getByText(newTask));
+
+    // check if the task has been included on the table
+    expect(tdNode).toBeDefined();
+  });
+});
